a9f9d2ab3ae2dc90328f0e3f4dd4db822e6b4fc7
HieuDo <minhhieu76qng@gmail.com>
2019-10-29 09:37:17 +0700

    finish home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
+import Homepage from './components/homepage/Homepage';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Homepage />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/footer/navFooter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const NavFooter = () => {
+  return (
+    <nav>
+      <ul className='nav-footer'>
+        <li>
+          <a className='link' href='#'>Home</a>
+        </li>
+        <li>
+          <a className='link' href='#'>Products</a>
+        </li>
+        <li>
+          <a className='link' href='#'>Services</a>
+        </li>
+        <li>
+          <a className='link' href='#'>About Us</a>
+        </li>
+        <li>
+          <a className='link' href='#'>Help</a>
+        </li>
+        <li>
+          <a className='link' href='#'>Contacts</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavFooter;
