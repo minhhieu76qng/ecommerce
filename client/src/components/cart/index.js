e09ec20dccf5353e61c8cdda4eccda02c612a6a2
HieuDo <minhhieu76qng@gmail.com>
2019-11-01 17:21:39 +0700

    add cart (button and price)

--- client/src/components/cart/index.js
+++ client/src/components/cart/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CartItem from './CartItem';
-import { Row, Col } from 'antd';
+import { Row, Col, Button, Divider } from 'antd';
 import './index.scss';
 
 const Cart = () => {
@@ -8,7 +8,7 @@ const Cart = () => {
     <div id='cart'>
       <h3 className='title'>My Bag</h3>
       <Row className='table-product' gutter={20} style={{ marginTop: 30 }}>
-        <Col span={17}>
+        <Col span={16}>
           <Row className='table-header' gutter={20}>
             <Col span={9}>Product</Col>
             <Col span={3} style={{ textAlign: 'center' }}>
@@ -32,7 +32,26 @@ const Cart = () => {
             <CartItem />
           </ul>
         </Col>
-        <Col span={7}>f</Col>
+        <Col span={1} />
+        <Col span={7}>
+          <div className='table-header'>Total</div>
+          <div className='checkout-block'>
+            <div className='row'>
+              <p>Shipping &amp; Handling:</p>
+              <p>Free</p>
+            </div>
+            <div className='row'>
+              <p>Total product:</p>
+              <p>$6.900</p>
+            </div>
+            <Divider />
+            <div className='row' style={{ fontWeight: 700, fontSize: 16 }}>
+              <p>Subtotal:</p>
+              <p>$6.900</p>
+            </div>
+          </div>
+          <Button block className='btn' type='danger' size='large' style={{ marginTop: 20 }}>Check out</Button>
+        </Col>
       </Row>
     </div>
   );
